c9dace5f630f3cde6900ca546d779cf87cac0bf5
MmmarRTHa <m27842n@outlook.com>
2025-04-15 22:23:43 -0700

    Initial commit for echobot ai app in vuejs

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+    <h1 class="text-2xl bg-amber-600">Hello World</h1>
+</template>
+
+<style scoped></style>
