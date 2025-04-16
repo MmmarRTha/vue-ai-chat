b6c729306b35dbc3a2c0aa1f4462ab28d70e5719
MmmarRTHa <m27842n@outlook.com>
2025-04-15 23:17:08 -0700

    - update: feature/footer - add footer component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
 import LogoHeader from './components/LogoHeader.vue'
+import Footer from './components/Footer.vue'
 </script>
 
 <template>
-    <LogoHeader />
+    <main class="max-w-3xl mx-auto my-4 px-4 sm:px-6 lg:px-8" role="main">
+        <LogoHeader />
+        <Footer />
+    </main>
 </template>
 
 <style scoped></style>
